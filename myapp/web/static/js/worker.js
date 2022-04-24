a6015a1a516d2d8df3a32e316ce35d1ea491a70e
Madugba Emmanuel Chinonso <nwadubaemmanuel@gmail.com>
2022-04-24 14:37:13 -0700

    update selection box

--- myapp/web/static/js/worker.js
+++ myapp/web/static/js/worker.js
@@ -3,30 +3,29 @@ const selectState = document.getElementById('stud_state');
 const selectCity = document.getElementById('stud_city');
 
 selectState.addEventListener('change', (event) => {
-event.preventDefault();
-const state = selectState.value;
-selectCity.innerHTML = '<option value="">Please wait...</option>';
-$.ajax({
-    url: `https://www.universal-tutorial.com/api/cities/${state}`,
-    type: 'GET',
-    dataType: 'json',
-    success: (data) => {
-        console.log(data);
-        selectCity.innerHTML = '<option value="">Select City</option>';
-        data.forEach(state_name => {
-            const option = document.createElement('option');
-            option.value = state_name;
-            option.textContent = state_name;
-            selectCity.appendChild(option);
-        });
-    },
+    event.preventDefault();
+    const state = selectState.value;
+    selectCity.innerHTML = '<option value="">Please wait...</option>';
+    $.ajax({
+        url: `https://www.universal-tutorial.com/api/cities/${state}`,
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            console.log(data);
+            selectCity.innerHTML = '<option value="">Select City</option>';
+            data.forEach(state_name => {
+                const option = document.createElement('option');
+                option.value = state_name;
+                option.textContent = state_name;
+                selectCity.appendChild(option);
+            });
+        },
 
-    fail: (error) => {
-        selectCity.innerHTML = '<option value="">Select City</option>';
-        console.log(error);
-    }
-});
-});
+        fail: (error) => {
+            selectCity.innerHTML = '<option value="">Select City</option>';
+            console.log(error);
+        }
+    });
 
 
 
